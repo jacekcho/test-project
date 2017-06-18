25b86a5f9705dc0761ee3e56e965fdedb2664de2
jacek.chowaniec <jacek.chowaniec85@gmail.com>
2017-06-18 10:51:08 +0200

    refactoring in pom.xml

--- src/test/java/com/demoqa/bdd/tests/LogInPageSteps.java
+++ src/test/java/com/demoqa/bdd/tests/LogInPageSteps.java
@@ -6,8 +6,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-import static org.junit.Assert.assertThat;
-
 public class LogInPageSteps {
 
     private LoginPage loginPage = new LoginPage();
